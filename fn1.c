dbdf0012d92d49fa8af06ccd7f6654861c9dd960
ruizdani301 <ruizdani301@gmail.com>
2022-01-13 21:46:52 -0500

    1

--- fn1.c
+++ fn1.c
@@ -90,7 +90,8 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if ((tmp == NULL) || (tmp->next == NULL))
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n",
+			line_number);
 		exit(EXIT_FAILURE);
 	}
 
