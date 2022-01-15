86d8c340b43685818fd24c3f2c5833ea40818283
ruizdani301 <ruizdani301@gmail.com>
2022-01-15 04:39:41 -0500

    se agrega funcion rotl

--- rotl.c
+++ rotl.c
@@ -1,20 +1,21 @@
 #include "monty.h"
 
 /**
- * swap - swaps the top two elements of the stack.
+ * rotl - turn top to the last in the stack.
  * @stack: struct stack_t
  * @line_number: line number to show in error case
  */
 void rotl(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
+
 	if ((tmp == NULL) || (tmp->next == NULL))
 	{
 		return;
 	}
 	(void)line_number;
-  	while (tmp->next != NULL)
-  	{
+	while (tmp->next != NULL)
+	{
 		tmp = tmp->next;
 	}
 	tmp->next = *stack;
