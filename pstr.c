5ade7f3caac8b3e3dbf46dda50a311fa38641fbd
ruizdani301 <ruizdani301@gmail.com>
2022-01-14 19:49:05 -0500

    pstr

---
+++ pstr.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * pstr - turn the list;
+ * @stack: head (stack) to the stack;
+ * @line_number: line number where opcode is located
+ */
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+	int str = 0;
+
+	while (tmp->next != NULL && tmp->n != 0)
+	{
+		str = tmp->n;
+		if (str > 0 && str < 127)
+		{
+			putchar(str);
+		}
+		else
+		{
+			break;
+		}
+		tmp = tmp->next;
+	}
+	putchar(10);
+	(void)line_number;
+}
