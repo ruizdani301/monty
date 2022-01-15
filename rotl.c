c1b4688753025e7ad3ad496ffaee2116266b6b34
ruizdani301 <ruizdani301@gmail.com>
2022-01-15 04:32:55 -0500

    linea 16

---
+++ rotl.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * swap - swaps the top two elements of the stack.
+ * @stack: struct stack_t
+ * @line_number: line number to show in error case
+ */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+	if ((tmp == NULL) || (tmp->next == NULL))
+	{
+		return;
+	}
+	(void)line_number;
+  	while (tmp->next != NULL)
+  	{
+		tmp = tmp->next;
+	}
+	tmp->next = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+	tmp->next->prev = tmp;
+	tmp = tmp->next;
+}
