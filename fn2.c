30631c26a6fd0089cb4fa1083304838fd2cd845e
ruizdani301 <ruizdani301@gmail.com>
2022-01-14 21:00:00 -0500

    se corrigen espacios

--- fn2.c
+++ fn2.c
@@ -11,7 +11,6 @@ void pop(stack_t **stack, unsigned int line_number)
 	stack_t *tmp;
 
 	tmp = *stack;
-	
 	if ((tmp) == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
